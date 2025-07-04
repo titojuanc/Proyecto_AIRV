662261b4557804cd80c23a6e066ee2c24e7e8ec1
Fyyysh <devmmruiz@gmail.com>
2025-07-04 13:42:58 -0300

    no me acuerdo q hice aca

--- Librerías/blutu.py
+++ Librerías/blutu.py
@@ -11,7 +11,7 @@ async def scan():
         print(f"Name: {d.name}, Address: {d.address}")
         i=i+1
         addresses.append(d.address)
-        asyncio.run(escuchar_responder.speak(f"Opción{i}: {d.name}"))
+        escuchar_responder.speak(f"Opción{i}: {d.name}")
         opcion=escuchar_responder.listen()
     match opcion:
         case "opción uno":
@@ -25,9 +25,10 @@ async def scan():
         case "opción cinco":
             device_adress= addresses[4]
         case _:
-            asyncio.run(escuchar_responder.speak(f"No entendí"))
+            escuchar_responder.speak(f"No entendí")
 
     asyncio.run(connect(device_adress))
+    
 async def connect(address):
     async with BleakClient(address) as client:
         if await client.is_connected():
