a32a415814b66903b35f3a18298bc5c4de581280
Fyyysh <devmmruiz@gmail.com>
2025-07-04 14:10:30 -0300

    hecho ayuda de sonido y boludeces mas

--- Librerías/menuSonido.py
+++ Librerías/menuSonido.py
@@ -3,7 +3,7 @@ import escuchar_responder
 import asyncio
 
 def menuSonido():
-    """Voice-controlled menu for sound settings"""
+    #Voice-controlled menu for sound settings
     while True:
         teto = escuchar_responder.listen()
         if teto:
@@ -17,12 +17,14 @@ def menuSonido():
                 case "desmutear":
                     sonido.unmute()
                 case "ajustar volumen":
-                    asyncio.run(escuchar_responder.speak("¿Qué nivel de volumen? (0-100)"))
+                    asyncio.run(escuchar_responder.speak("¿Qué nivel de volumen? del cero al 100"))
                     volumen = escuchar_responder.listen()
                     sonido.set_volume(volumen)
                 case "salir":
                     break
+                case "ayuda":
+                    asyncio.run(escuchar_responder.speak("Para subir el volumen decir: subir volumen. para bajar el volumen decir: bajar volumen. Para sileciar decir: mutear. Para desilenciar decir: desmutear. Para ajustar el volumen a un valor personalizado deseado decir: ajustar volumen. Para salir del modo de sonido decir: salir"))
                 case _:
-                    asyncio.run(escuchar_responder.speak("No se ha entendido la orden."))
+                    asyncio.run(escuchar_responder.speak("No se ha entendido la orden. Si precisa ayuda diga: Ayuda."))
 
 menuSonido()
